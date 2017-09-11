a63a5f4a8afd093363f16b8232b1acc7f390e82c
CrazyLucker <xxxcorsair@yandex.ru>
2017-09-11 13:34:03 +0300

    version 1.1

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -1,16 +1,6 @@
 package com.company;
 
 public class Field {
-    /*private int[] backendField = new int[9];
-
-
-    public int getBackendFieldElement(int element){
-        return backendField[element];
-    }
-
-    public void setBackendFieldElement(int element, int Integer){
-        backendField[element] = Integer;
-    }*/
     private String[] field = new String[9];
 
     public String getFieldElement(int element) {
